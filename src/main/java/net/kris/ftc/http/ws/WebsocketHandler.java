36583602adbae390db4f1c4d1987db1d33fd0f03
KrisanaponMeksamoot <kriskrisanapon@gmail.com>
2023-07-05 10:58:57 +0700

    add websocket

---
+++ src/main/java/net/kris/ftc/http/ws/WebsocketHandler.java
@@ -0,0 +1,90 @@
+package net.kris.ftc.http.ws;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import net.kris.ftc.http.ClientConnection;
+import net.kris.ftc.http.HttpRequest;
+import net.kris.ftc.http.HttpResponse;
+
+public class WebsocketHandler {
+    ClientConnection con;
+    DataInputStream dis;
+    DataOutputStream dos;
+
+    int version;
+
+    public WebsocketHandler(ClientConnection con) throws IOException {
+        this.con = con;
+        this.dis = new DataInputStream(con.getInputStream());
+        this.dos = new DataOutputStream(con.getOutputStream());
+    }
+
+    public void sendHeader() throws IOException {
+        HttpRequest req = con.getRequest();
+        HttpResponse res = con.getResponse();
+        assert isWebsocket(req);
+        res.putAttribute("Sec-Websocket-Accept", WSUtils.keyAccept(req.getAttributes().get("Sec-Websocket-Key")));
+        version = Integer.valueOf(req.getAttributes().get("Sec-Websocket-Version"));
+        con.sendHeader();
+    }
+
+    public static boolean isWebsocket(HttpRequest req) throws IOException {
+        if (!req.getMethod().toUpperCase().equals("GET"))
+            return false;
+        if (!req.getAttributes().get("Connection").equals("Upgrade"))
+            return false;
+        if (!req.getAttributes().get("Upgrade").equals("websocket"))
+            return false;
+        if (!req.getAttributes().containsKey("Sec-Websocket-Key"))
+            return false;
+        return true;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void send(WSFrame packet) throws IOException {
+        dos.write((packet.fin ? 0x80 : 0) | packet.opcode);
+        int lenSize = packet.data.length > 125 ? (packet.data.length > 65535 ? 2 : 1) : 0;
+        dos.write(lenSize == 0 ? packet.data.length : (lenSize == 1 ? 126 : 127));
+        if (lenSize > 0) {
+            if (lenSize == 1) {
+                dos.writeShort(packet.data.length);
+            } else {
+                dos.writeLong(packet.data.length);
+            }
+        }
+        if (packet.isMask) {
+            dos.write(packet.maskKey);
+            WSUtils.mask(packet.data, packet.maskKey);
+        }
+        dos.write(packet.data);
+        dos.flush();
+    }
+
+    public WSFrame receive() throws IOException {
+        WSFrame packet = new WSFrame();
+        int b = dis.readByte();
+        packet.opcode = b & 0x0F;
+        packet.fin = (b & 0x80) == 0x80;
+        b = dis.readByte();
+        int length = b & 0x7F;
+        packet.isMask = (b & 0x80) == 0x80;
+        if (length == 126) {
+            packet.data = new byte[(int) dis.readShort() & 0xFFFF];
+        } else if (length == 127) {
+            packet.data = new byte[(int) dis.readLong()];
+        } else {
+            packet.data = new byte[length];
+        }
+        if (packet.isMask)
+            packet.maskKey = new byte[4];
+        dis.readFully(packet.maskKey);
+        dis.readFully(packet.data);
+        WSUtils.mask(packet.data, packet.maskKey);
+        return packet;
+    }
+}
